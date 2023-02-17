27ea668cdc059e6579744f84bbe7cd3fa671e1a9
Danil <danilkozlov1003@gmail.com>
2023-02-17 19:13:48 +0700

    Добавил иконки рангов

---
+++ src/main/java/ru/wiki/dotainf/utilities/HeroesSearch.java
@@ -0,0 +1,13 @@
+package ru.wiki.dotainf.utilities;
+
+public class HeroesSearch {
+    private String searchText;
+
+    public String getSearchText() {
+        return searchText;
+    }
+
+    public void setSearchText(String searchText) {
+        this.searchText = searchText;
+    }
+}
