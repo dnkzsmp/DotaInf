444db19594bc2ede33ff1f39cce3ae19d2ecc240
Danil <danilkozlov1003@gmail.com>
2023-02-17 19:17:46 +0700

    Реализовал POST-ручку для поиска героев по введенной строке

--- src/main/java/ru/wiki/dotainf/controller/entities/HeroesController.java
+++ src/main/java/ru/wiki/dotainf/controller/entities/HeroesController.java
@@ -3,13 +3,12 @@ package ru.wiki.dotainf.controller.entities;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import ru.wiki.dotainf.controller.api.reader.JsonReader;
+import org.springframework.web.bind.annotation.*;
+import ru.wiki.dotainf.reader.JsonReader;
 import ru.wiki.dotainf.database.tables.Heroes;
 import ru.wiki.dotainf.database.repository.HeroesRepository;
 import ru.wiki.dotainf.utilities.HeroStats;
+import ru.wiki.dotainf.utilities.HeroesSearch;
 
 import java.net.URL;
 import java.util.List;
@@ -24,12 +23,20 @@ public class HeroesController {
     public String heroes(Model model) {
         List<Heroes> heroes = heroesRepository.findAll();
         model.addAttribute("heroes", heroes);
+        model.addAttribute("heroesSearch", new HeroesSearch());
+        return "entities/heroes";
+    }
+
+    @PostMapping
+    public String heroesSearch(@ModelAttribute HeroesSearch heroesSearch, Model model) {
+        List<Heroes> heroes = heroesRepository.findByHeroNameContaining(heroesSearch.getSearchText());
+        model.addAttribute("heroes", heroes);
         return "entities/heroes";
     }
 
     @GetMapping("/hero-stats/{heroName}")
     public String heroStats(@PathVariable("heroName") String heroName, Model model) throws Exception {
-        HeroStats heroStats = JsonReader.getHeroFromApiJson(
+        HeroStats heroStats = JsonReader.getHeroStatsFromApiJson(
                 heroName,
                 new URL("https://api.opendota.com/api/heroStats")
         );
