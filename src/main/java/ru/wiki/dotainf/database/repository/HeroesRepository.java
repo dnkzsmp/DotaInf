2f00e0e0571a440dab4572f77ed6e72d552e148a
Danil <danilkozlov1003@gmail.com>
2023-02-17 19:18:19 +0700

    Добавил метод для поиска героев по содержанию строки в их имени

--- src/main/java/ru/wiki/dotainf/database/repository/HeroesRepository.java
+++ src/main/java/ru/wiki/dotainf/database/repository/HeroesRepository.java
@@ -3,5 +3,8 @@ package ru.wiki.dotainf.database.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.wiki.dotainf.database.tables.Heroes;
 
+import java.util.List;
+
 public interface HeroesRepository extends JpaRepository<Heroes, Long> {
+    List<Heroes> findByHeroNameContaining(String infix);
 }
