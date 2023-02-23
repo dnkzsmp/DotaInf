adedd42cdd115a2afee5879a8b0340af9b7f0651
Danil <danilkozlov1003@gmail.com>
2023-02-23 22:01:42 +0700

    Добавил эндпоинты для hero-matches

--- src/main/java/ru/wiki/dotainf/controller/api/HeroesApiController.java
+++ src/main/java/ru/wiki/dotainf/controller/api/HeroesApiController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.*;
 import ru.wiki.dotainf.reader.JsonReader;
 import ru.wiki.dotainf.database.tables.Heroes;
 import ru.wiki.dotainf.database.repository.HeroesRepository;
-import ru.wiki.dotainf.utilities.HeroStats;
+import ru.wiki.dotainf.utilities.hero.HeroMatches;
+import ru.wiki.dotainf.utilities.hero.HeroMatchups;
+import ru.wiki.dotainf.utilities.hero.HeroStats;
 
 import java.net.URL;
 import java.util.List;
@@ -33,20 +35,56 @@ public class HeroesApiController {
         }
     }
 
-    @GetMapping("/hero-stats/{heroName}")
-    public ResponseEntity<HeroStats> getHeroStatsById(@PathVariable(value = "heroName") String heroName) {
+    @GetMapping("/hero-stats/{id}")
+    public ResponseEntity<HeroStats> getHeroStatsById(@PathVariable(value = "id") Integer id) {
         try {
-            if (heroName == null) {
+            if (id == null) {
                 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
             }
             HeroStats heroStats = JsonReader.getHeroStatsFromApiJson(
-                    heroName,
+                    id,
                     new URL("https://api.opendota.com/api/heroStats")
             );
             if (heroStats == null) {
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
             return new ResponseEntity<>(heroStats, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/hero-matches/leagues/{id}")
+    public ResponseEntity<List<HeroMatches>> getHeroLeagueMatchesById(@PathVariable(value = "id") Integer id)  {
+        try {
+            if (id == null) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            List<HeroMatches> heroMatches = JsonReader.getHeroMatchesFromApi(
+                    new URL("https://api.opendota.com/api/heroes/" + id + "/matches")
+            );
+            if (heroMatches.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(heroMatches, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/hero-matches/matchups/{id}")
+    public ResponseEntity<List<HeroMatchups>> getHeroMatchupsById(@PathVariable(value = "id") Integer id) {
+        try {
+            if (id == null) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            List<HeroMatchups> heroMatchups = JsonReader.getHeroMatchupsFromApi(
+                    new URL("https://api.opendota.com/api/heroes/" + id + "/matchups")
+            );
+            if (heroMatchups.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(heroMatchups, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
