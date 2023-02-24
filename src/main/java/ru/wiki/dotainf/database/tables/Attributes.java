e5df9f49a635ff9a89f77a329db211a54af3b3c0
Danil <danilkozlov1003@gmail.com>
2023-02-24 14:27:39 +0700

    Добавил сущность таблицы компонентов

---
+++ src/main/java/ru/wiki/dotainf/database/tables/Attributes.java
@@ -0,0 +1,86 @@
+package ru.wiki.dotainf.database.tables;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "attribs")
+public class Attributes {
+    @Id
+    private Integer id;
+    @Column(name = "itemid")
+    private Integer itemid;
+
+    @Column(name = "key")
+    private String key;
+
+    @Column(name = "header")
+    private String header;
+
+    @Column(name = "value")
+    private String value;
+
+    @Column(name = "footer")
+    private String footer;
+
+    public Attributes() {}
+
+    public Attributes(Integer id, Integer itemid, String key, String header, String value, String footer) {
+        this.id = id;
+        this.itemid = itemid;
+        this.key = key;
+        this.header = header;
+        this.value = value;
+        this.footer = footer;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getItemid() {
+        return itemid;
+    }
+
+    public void setItemid(Integer itemid) {
+        this.itemid = itemid;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getFooter() {
+        return footer;
+    }
+
+    public void setFooter(String footer) {
+        this.footer = footer;
+    }
+}
---
+++ src/main/java/ru/wiki/dotainf/database/tables/Components.java
@@ -0,0 +1,49 @@
+package ru.wiki.dotainf.database.tables;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "components")
+public class Components {
+    @Id
+    private Integer id;
+    @Column(name = "itemid")
+    private Integer itemid;
+    @Column(name = "name")
+    private String name;
+
+    public Components() {}
+
+    public Components(Integer id, Integer itemid, String name) {
+        this.id = id;
+        this.itemid = itemid;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getItemid() {
+        return itemid;
+    }
+
+    public void setItemid(Integer itemid) {
+        this.itemid = itemid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
